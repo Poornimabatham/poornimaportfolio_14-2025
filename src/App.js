2bbbfbe67ca37b15eb85b7211150396dbda0a7bc
Poornima Batham <pbatham21@gmail.com>
2025-04-25 21:20:48 +0530

    addc deo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import Education from "./components/Education";
@@ -7,31 +10,34 @@ import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Footer from "./components/Footer";
+
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // animation duration
+      once: true, // animation only once on scroll
+    });
+  }, []);
+
   return (
     <>
       <Navbar />
-
-      <div id="about">
-        {" "}
+      <div id="about" data-aos="fade-up">
         <HeroSection />
       </div>
-      <div id="education">
+      <div id="education" data-aos="fade-up">
         <Education />
       </div>
-      <div id="experience">
-        {" "}
+      <div id="experience" data-aos="fade-up">
         <Experience />
       </div>
-      <div id="skills">
+      <div id="skills" data-aos="fade-up">
         <Skills />
       </div>
-      <div id="projects">
-        {" "}
+      <div id="projects" data-aos="fade-up">
         <Projects />
       </div>
-      <div id="footer">
-        {" "}
+      <div id="footer" data-aos="fade-up">
         <Footer />
       </div>
     </>
